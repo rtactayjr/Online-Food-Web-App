f260c0b6f79bf31617b3ec39cebe179b5db307d2
r-tactay <rolandtactay571@gmail.com>
2023-09-08 20:26:49 +0800

    Update location function: Get the user current location and organize homepage to show nearby restaurants

--- Online_Food_Web_App_Main/views.py
+++ Online_Food_Web_App_Main/views.py
@@ -4,14 +4,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
+from django.contrib.gis.db.models.functions import Distance
+
+
 from merchant.models import Merchant
 
 #####################
 # defined functions #
 #####################
+def get_or_set_current_location(request):
+    if 'lat' in request.session:
+        lat = request.session['lat']
+        lng = request.session['lng']
+        return lng, lat
+    elif 'lat' in request.GET:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+        request.session['lat'] = lat
+        request.session['lng'] = lng
+        return lng, lat
+    else:
+        return None
+    
 def home(request):
-    # Retrieve a list of approved and active merchants (up to 8) for display on the home page
-    merchants = Merchant.objects.filter(is_approved=True, user__is_active=True)[:8]
+    if get_or_set_current_location(request) is not None:
+        pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
+        merchants = Merchant.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+        for m in merchants:
+            m.kms = round(m.distance.km, 1)
+    else:
+        # Retrieve a list of approved and active merchants (up to 8) for display on the home page
+        merchants = Merchant.objects.filter(is_approved=True, user__is_active=True)[:8]
   
     # Create a context dictionary to pass data to the template
     context = {

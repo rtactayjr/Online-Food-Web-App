165ca94b5d4efc1a48948e34a59f7ea7318ad8a1
r-tactay <rolandtactay571@gmail.com>
2023-09-10 20:50:47 +0800

    Implement Paypal Payment Gateway

--- Online_Food_Web_App_Main/settings.py
+++ Online_Food_Web_App_Main/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amounts',
                 'accounts.context_processors.get_user_profile',
-                # 'accounts.context_processors.get_paypal_client_id',
+                'accounts.context_processors.get_paypal_client_id',
             ],
         },
     },
@@ -175,3 +175,7 @@ GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 os.environ['PATH'] = os.path.join(BASE_DIR, 'venv\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
 os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'venv\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
 GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'venv\Lib\site-packages\osgeo\gdal304.dll')
+
+PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
--- orders/views.py
+++ orders/views.py
@@ -115,8 +115,97 @@ def place_order(request):
     return render(request, 'orders/place_order.html',)
 
 
+@login_required(login_url='login')
 def payments(request):
-    pass
+        # Check if the request is ajax or not
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':
+        # STORE THE PAYMENT DETAILS IN THE PAYMENT MODEL
+        order_number = request.POST.get('order_number')
+        transaction_id = request.POST.get('transaction_id')
+        payment_method = request.POST.get('payment_method')
+        status = request.POST.get('status')
+
+        order = Order.objects.get(user=request.user, order_number=order_number)
+        payment = Payment(
+            user = request.user,
+            transaction_id = transaction_id,
+            payment_method = payment_method,
+            amount = order.total,
+            status = status
+        )
+        payment.save()
+
+        # UPDATE THE ORDER MODEL
+        order.payment = payment
+        order.is_ordered = True
+        order.save()
+
+        # MOVE THE CART ITEMS TO ORDERED FOOD MODEL
+        cart_items = Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            ordered_food = OrderedFood()
+            ordered_food.order = order
+            ordered_food.payment = payment
+            ordered_food.user = request.user
+            ordered_food.product_item = item.product_item
+            ordered_food.quantity = item.quantity
+            ordered_food.price = item.product_item.price
+            ordered_food.amount = item.product_item.price * item.quantity # total amount
+            ordered_food.save()
+
+        # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
+        mail_subject = 'Thank you for ordering with us.'
+        mail_template = 'orders/order_confirmation_email.html'
+
+        ordered_food = OrderedFood.objects.filter(order=order)
+        customer_subtotal = 0
+        for item in ordered_food:
+            customer_subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'user': request.user,
+            'order': order,
+            'to_email': order.email,
+            'ordered_food': ordered_food,
+            'domain': get_current_site(request),
+            'customer_subtotal': customer_subtotal,
+            'tax_data': tax_data,
+        }
+        send_notification(mail_subject, mail_template, context)
+        
+
+        # SEND ORDER RECEIVED EMAIL TO THE merchant
+        mail_subject = 'You have received a new order.'
+        mail_template = 'orders/new_order_received.html'
+        to_emails = []
+        for i in cart_items:
+            if i.product_item.merchant.user.email not in to_emails:
+                to_emails.append(i.product_item.merchant.user.email)
+
+                ordered_food_to_merchant = OrderedFood.objects.filter(order=order, product_item__merchant=i.product_item.merchant)
+                print(ordered_food_to_merchant)
+
+        
+                context = {
+                    'order': order,
+                    'to_email': i.product_item.merchant.user.email,
+                    'ordered_food_to_merchant': ordered_food_to_merchant,
+                    'merchant_subtotal': order_total_by_merchant(order, i.product_item.merchant.id)['subtotal'],
+                    'tax_data': order_total_by_merchant(order, i.product_item.merchant.id)['tax_dict'],
+                    'merchant_grand_total': order_total_by_merchant(order, i.product_item.merchant.id)['grand_total'],
+                }
+                send_notification(mail_subject, mail_template, context)
+
+        # CLEAR THE CART IF THE PAYMENT IS SUCCESS
+        # cart_items.delete() 
+
+        # RETURN BACK TO AJAX WITH THE STATUS SUCCESS OR FAILURE
+        response = {
+            'order_number': order_number,
+            'transaction_id': transaction_id,
+        }
+        return JsonResponse(response)
+    return HttpResponse('Payments view')
 
 def order_complete(request):
     pass
